d15b6e895be1d3bdc9eae7865dd5c8b658b69af9
Scott Robertson <scott.t.robertson@gmail.com>
2024-04-04 18:23:36 -0400

    division note added

--- src/views/LeaderboardView.vue
+++ src/views/LeaderboardView.vue
@@ -5,6 +5,11 @@ import Leaderboard from '../components/Leaderboard.vue';
 <template>
     <h1>Leaderboard</h1>
     <Leaderboard />
+    <h2>Divisions</h2>
+    <p>
+        Divisions do exist, unfortunately, your humble solo web developer hasn't
+        had a chance to implement the feature. Stay tuned! It'll be fun.
+    </p>
 </template>
 
 <style scoped></style>
