3f351c71a620371384ef6e014e262f2419db1ce6
Scott Robertson <scott.t.robertson@gmail.com>
2024-04-13 12:50:04 -0400

    added new twitch overlays

---
+++ src/views/TwitchLeaderboardView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Leaderboard from '../components/Leaderboard.vue';
+import Meet from '../components/Meet.vue';
+import { MeetSchema } from '../schemas/meet';
+import { queryMeet } from '../queries/meets';
+
+document.body.classList.add('twitch');
+
+const meets = ref<MeetSchema[]>();
+queryMeet().then((res) => {
+    meets.value = res;
+});
+
+</script>
+
+<template>
+    <Leaderboard />
+</template>
+
+<style lang="scss">
+.twitch{
+
+    #app {
+        background: #00ff00;
+    }
+    .nav-container {
+        display: none!important;
+    }
+    footer {
+        display: none!important;
+    }
+}
+</style>
